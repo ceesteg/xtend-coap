cb15918d739ed900ee10dbfa7681f966bd3424e8
César Estebas Gómez <ceesteg@gmail.com>
2014-09-07 20:46:05 +0200

    Xtend-CoAP.jar library created.

--- Xtend-CoAP/src/main/generated-sources/xtend/com/xtend/coap/index/Index.java
+++
@@ -1,8 +0,0 @@
-package com.xtend.coap.index;
-
-@SuppressWarnings("all")
-public class Index {
-  public static void main(final String[] args) {
-    System.out.println("This is the CoAP protocol implemented in Xtend!!!");
-  }
-}
